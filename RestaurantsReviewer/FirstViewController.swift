4d43726d18fd24e73a8a222941c40113ba121a19
Derek Townsend <derekjavon2@gmail.com>
2020-04-29 23:44:10 -0500

    Initial Commit

---
+++ RestaurantsReviewer/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  RestaurantsReviewer
+//
+//  Created by Derek Townsend on 4/29/20.
+//  Copyright © 2020 Derek Townsend. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RestaurantsReviewer/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  RestaurantsReviewer
+//
+//  Created by Derek Townsend on 4/29/20.
+//  Copyright © 2020 Derek Townsend. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
